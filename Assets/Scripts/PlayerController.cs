072dc3996da009ea728dff26a57b728c1b69ff1f
Joana Arezes <joanarezes@outlook.com>
2018-11-20 23:15:23 +0000

    Fix player movement, problem with drawing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    bool can_move;
+
 	// Use this for initialization
 	void Start () {
+        can_move = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float h = Input.GetAxis("Horizontal") * 0.1f;
-        float v = Input.GetAxis("Vertical") * 0.1f;
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.JoystickButton1))
+        {
+            if (can_move)
+                can_move = false;
+            else
+                can_move = true;
+        }
 
-        Vector3 forward = Camera.main.transform.forward;
-        forward.y = 0;
-        Vector3 right = Camera.main.transform.right;
-        right.y = 0;
+        if (can_move)
+        {
+            float h = Input.GetAxis("Horizontal") * 0.1f;
+            float v = Input.GetAxis("Vertical") * 0.1f;
 
-        Vector3 delta = forward * v + right * h;
+            Vector3 forward = Camera.main.transform.forward;
+            forward.y = 0;
+            Vector3 right = Camera.main.transform.right;
+            right.y = 0;
 
-        transform.position += delta;
+            Vector3 delta = forward * v + right * h;
+
+            transform.position += delta;
+        }
     }
 }
