3c35846ab7816657b00252bceed0a20d32d215d7
Joana Arezes <joanarezes@outlook.com>
2018-11-20 15:21:27 +0000

    Add more files...

---
+++ Assets/Scripts/CastSpellControler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastSpellControler : MonoBehaviour {
+
+    public GameObject spell;
+    private Ray direction;
+    private RaycastHit hit;
+
+    public float speed = 0.2f;
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.JoystickButton1) ||Input.GetKeyDown("a")){
+
+            Ray dir = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0));
+            GameObject clone = Instantiate(spell, transform.position, transform.rotation);
+            clone.GetComponent<SpellControler>().direction = dir.direction.normalized; 
+
+        }
+
+	}
+}
---
+++ Assets/Scripts/SpellControler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellControler : MonoBehaviour {
+
+    public Vector3 direction;
+    public GameObject impactPS;
+    public GameObject reticleDirection;
+    public float time = 0;
+    public bool impact;
+    public Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+        impact = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if (impact){
+            impactPS.SetActive(true);
+
+            Destroy(gameObject);
+        }
+        if(time >= 5.0){
+            Destroy(gameObject);
+        }
+        transform.position += direction * 0.5f;
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+        impact = true;
+	}
+
+}
